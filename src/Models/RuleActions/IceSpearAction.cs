0ac15d0bbaa56ea4f2f418a86cd898181644183a
ArchLeaders <archparliament@gmail.com>
2023-04-19 14:39:37 -0700

    Create IceSpear action

---
+++ src/Models/RuleActions/IceSpearAction.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using IceSpearConfig = System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, System.Text.Json.JsonElement>>;
+
+namespace UkmmFlash.Models.RuleActions;
+
+public sealed class IceSpearAction : RuleAction
+{
+    public override string Description { get; } = """
+        Copies the targeted map unit files from Ice-Spear into your mod folder before deploying.
+
+        Note: The search pattern must include the Ice-Spear project name.
+        """;
+
+    private static readonly string? _configPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".ice-spear", "config.json");
+    private static readonly string? _path = Load()?["projects"]["path"].GetString();
+    private static IceSpearConfig? Load()
+    {
+        if (File.Exists(_configPath)) {
+            using FileStream fs = File.OpenRead(_configPath);
+            return JsonSerializer.Deserialize<IceSpearConfig>(fs);
+        }
+
+        return null;
+    }
+
+    public override bool IsEnabled => File.Exists(_configPath);
+
+    public override void Deploy(string path)
+    {
+
+    }
+}
